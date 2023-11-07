7140b1e14497317970783f2d895ab27dd2f0c243
MarcSans6 <marcsanscormenzana@gmail.com>
2023-11-07 12:38:04 +0100

    w

--- Assets/_Scripts/Teleportable.cs
+++ Assets/_Scripts/Teleportable.cs
@@ -12,15 +12,11 @@ public abstract class Teleportable: MonoBehaviour
     public float m_PortalOffset = 0.8f;
     protected Vector3 m_Forward;
 
-    protected Vector3 m_RotatedForward;
 
     public virtual bool CanTeleport(Portal _Portal)
     {
-        Debug.Log("CanTleporti=");
-        m_RotatedForward = _Portal.m_OtherPortal.rotation * transform.forward;
-        float l_DotAngle = Vector3.Dot(_Portal.m_OtherPortal.forward, m_RotatedForward);
+        float l_DotAngle = Vector3.Dot(_Portal.m_OtherPortal.forward, m_Forward);
         return l_DotAngle > m_DotTraversePortal && _Portal.m_MirrorPortal.isActiveAndEnabled;
-        return false;
     }
 
     public virtual void Teleport(Portal _Portal)
@@ -30,23 +26,6 @@ public abstract class Teleportable: MonoBehaviour
         Vector3 l_LocalMovement = _Portal.m_OtherPortal.InverseTransformDirection(m_Forward);
         Vector3 l_WorldMovement = _Portal.m_MirrorPortal.transform.TransformDirection(l_LocalMovement);
         transform.position = l_WorldPosition + l_WorldMovement * m_PortalOffset;
-    }
-
-    private void Update()
-    {
-        Debug.DrawLine(transform.position, m_Forward * 3);
-        Gizmos.color = Color.blue;
-        Debug.DrawLine(transform.position, m_RotatedForward * 3);
-    }
-
-    private void OnDrawGizmos()
-    {
-        Vector3 startPos1 = transform.position + Vector3.up * 1;
-        Vector3 startPos2 = transform.position + Vector3.up * 2;
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(startPos1, startPos1 + m_Forward * 3);
-        Gizmos.color = Color.blue;
-        Gizmos.DrawLine(startPos2, startPos2 + m_RotatedForward * 3);
     }
 }
 
