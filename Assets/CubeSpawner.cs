999172ff0c9a9d562b1f4fb9b5e4e3f75139736b
PirisAran <apiris@edu.tecnocampus.cat>
2023-11-10 17:38:50 +0100

    Cross Dispensor

---
+++ Assets/CubeSpawner.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour
+{
+    [SerializeField]
+    Transform m_SpawnPoint;
+    [SerializeField]
+    GameObject m_SpawnCube;
+
+    KeyCode m_InteractKeyCode = KeyCode.E;
+
+    bool m_PlayerInSpawnZone;
+
+    GameObject m_InstanceCube;
+
+    private void Awake()
+    {
+    }
+
+    void Update()
+    {
+        if (m_PlayerInSpawnZone && Input.GetKeyDown(m_InteractKeyCode))
+        {
+            SpawnCube();
+        }   
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == GameController.GetGameController().m_Player.transform)
+        {
+            m_PlayerInSpawnZone = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform == GameController.GetGameController().m_Player.transform)
+        {
+            m_PlayerInSpawnZone = true;
+        }
+    }
+
+    private void SpawnCube()
+    {
+        if (m_InstanceCube != null)
+        {
+            Destroy(m_InstanceCube);
+        }
+        m_InstanceCube = Instantiate(m_SpawnCube);
+        m_InstanceCube.transform.position = m_SpawnPoint.transform.position;
+    }
+
+}
---
+++ Assets/PortalCrosshair.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalCrosshair : MonoBehaviour
+{
+    [SerializeField] Portal m_BluePortal;
+    [SerializeField] Portal m_OrangePortal;
+
+    [SerializeField] GameObject m_DefaultCrosshair;
+    [SerializeField] GameObject m_BlueCrosshair;
+    [SerializeField] GameObject m_OrangeCrosshair;
+
+    private void Start()
+    {
+        m_DefaultCrosshair.SetActive(true);
+    }
+    private void Update()
+    {
+        
+    }   
+
+    private void CheckActivePortals()
+    {
+      
+        m_BlueCrosshair.SetActive(m_BluePortal.IsPlaced);
+        m_OrangeCrosshair.SetActive(m_OrangePortal.IsPlaced);
+       
+    }
+
+}
