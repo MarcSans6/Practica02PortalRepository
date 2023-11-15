3d6e2f0b9bef064b2554cd7d68c35f5e0f721a32
MarcSans6 <marcsanscormenzana@gmail.com>
2023-11-15 23:00:53 +0100

    w

---
+++ Assets/LaserDetector.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserDetector : MonoBehaviour
+{
+    public Material m_LaseredMaterial;
+    public Material m_UnlaseredMaterial;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
